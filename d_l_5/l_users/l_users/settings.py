5d1e5c3e6362ac5a36f91435888e29b8ad1b07f9
dennykvg <69587852+dennykvg@users.noreply.github.com>
2021-11-30 07:19:03 +0400

    Update settings.py

--- d_l_5/l_users/l_users/settings.py
+++ d_l_5/l_users/l_users/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-+9t3m(+#ijrn&f0ijr3v)s^=x-rw=a^k^$@b#vubi)g*@i1#($
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dennykvg.pythonanywhere.com']
 
 
 # Application definition
